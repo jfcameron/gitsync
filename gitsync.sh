caa57718efdabb5cb4fea670eeb2fba197618ba3
Joseph Cameron <jf.cameron@gmail.com>
2017-12-08 03:07:13 -0500

    wip

--- gitsync.sh
+++ gitsync.sh
@@ -14,35 +14,37 @@ MAX_REPOS=100
 printHelp () {
 sed 's/^# \{0,1\}//' << Help
 #---------------------------------------------------------------------
-# Written by Joseph Cameron
-# Created on 2017-11-15.
 #
 # $progName -- help info 
 #
 # Summary:
-# Synchronize local copies of github account repos and starred repos.
-# Useful for maintaining a workspace across multiple machines.
-#
-# $shortProgName: [ -h clone pull push ]
-#       option: -h
-#         print this help page
-#
-#       option: clone [ -a/--all, -p/--personal, -s/--starred ]
-#         clone all (personal/starred/all) repos on your github
-#         account using curl/github api.
-#
-#       option: pull [ -a/--all, -p/--personal, -s/--starred ]
-#         git fetch --all;submodule update --init --recursive;git pull
-#         on all (personal/starred/all) repos in workspace
-#
-#       option: push []
-#         git add --all;git commit;git push all personal repos in 
-#         workspace.
+#   Synchronize local copies of github account repos and starred repos.
+#   Useful for maintaining a workspace across multiple machines.
 #
 # Example Usage:
-#       $shortProgName clone --all
-#       $shortProgName pull --personal
-#       $shortProgName push
+#   $shortProgName clone --all
+#   $shortProgName pull --personal
+#   $shortProgName push
+#
+# $shortProgName: [ -h clone pull push ]
+#   option: -h
+#     print this help page
+#
+#   option: clone [ -a/--all, -p/--personal, -s/--starred ]
+#     clone all (personal/starred/all) repos on your github
+#     account using curl/github api.
+#
+#   option: pull [ -a/--all, -p/--personal, -s/--starred ]
+#     git fetch --all;submodule update --init --recursive;git pull
+#     on all (personal/starred/all) repos in workspace
+#
+#   option: push []
+#     git add --all;git commit;git push all personal repos in 
+#     workspace.
+#
+# Author:
+#   Written by Joseph Cameron
+#   Created on 2017-11-15.
 #
 #---------------------------------------------------------------------
 Help
